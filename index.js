f6d5c471d5dfa11ca85b502d227493b0b51b6ac7
simpart <simpart>
2019-02-24 02:05:32 +0000

    del invclr

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 const mf     = require('mofron');
 const Header = require('mofron-comp-header');
 const Text   = require('mofron-comp-text');
-const Invclr = require('mofron-effect-invclr');
 const Synhei = require('mofron-effect-synchei');
+
 /**
  * @class mofron.comp.TxtHeader
  * @brief text header contents component for mofron
@@ -49,38 +49,26 @@ mf.comp.TxtHeader = class extends Header {
      * @param p1 (undefined) call as getter
      * @return text contents
      */
-    text (txt, dct) {
+    text (txt, lft) {
         try {
             if ('string' === typeof txt) {
-                this.text().execOption({ text : txt });
+                this.text().option({ text :txt });
+                if (undefined !== lft) {
+                    this.text().option({ style: {'margin-left' : lft} });
+                }
                 return;
-            } else if ( (true === mf.func.isComp(txt, 'Text')) && (true !== dct) ) {
-                txt.execOption({
-                    style  : [ {'margin-left' : '0.2rem'}, true],
-                    effect : [ new Synhei(this, '-0.2rem') ]
+            } else if (true === mf.func.isComp(txt, 'Text')) {
+                txt.option({
+                    style : [
+                        { 'margin-left' : (undefined === lft) ? '0.2rem' : lft },
+                        (undefined === lft) ? true : false
+                    ],
+                    effect: [
+                        new Synhei({ targetComp: this, offset: '-0.2rem', tag: this.name() + '-text' }),
+                    ]
                 });
             }
             return this.innerComp('text', txt, Text);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * setter/getter invert flag
-     * true flag changes text color when background color changed
-     * 
-     * @param flg (true) enable invert changing (default)
-     * @param flg (false) disable invert changing
-     * @return (boolean) invert flag
-     */
-    invert (flg) {
-        try {
-            let ret = this.effect('Invclr').suspend(
-                (undefined !== flg)? !flg : flg
-            );
-            return (undefined !== ret) ? !ret : ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -97,10 +85,10 @@ mf.comp.TxtHeader = class extends Header {
      */
     synchei (flg) {
         try {
-            let ret = this.effect('SyncHei').suspend(
-                (undefined !== flg)?  !flg : flg
+            let ret = this.text().effect(['SyncHei', this.name() + '-text']).suspend(
+                ('boolean' === typeof flg) ? !flg : undefined
             );
-            return (undefined !== ret) ? !ret : ret;
+            return (undefined !== ret) ? !ret: undefined;
         } catch (e) {
             console.error(e.stack);
             throw e;
